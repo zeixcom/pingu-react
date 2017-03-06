11a49c04cfe2599eaf8f674ccfa75f78c6c14c22
Robert Vogt <robert@smartive.ch>
2017-03-06 08:03:01 +0100

    Fix index file to make it compile

--- app/templates/index.js
+++ app/templates/index.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
 import createBrowserHistory from 'history/createBrowserHistory';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import { Provider } from 'react-redux';
 import createStoreWithMiddleware from './store';
-import { defaultState } from './defaultState';
+import { defaultState } from './default-state';
 
 // eslint-disable-next-line no-unused-vars
-import css from './assets/style.scss';
+import css from './assets/css/styles.scss';
 
 const browserHistory = createBrowserHistory();
 const store = createStoreWithMiddleware(browserHistory, defaultState);
 const history = syncHistoryWithStore(browserHistory, store);
 
 const App = () => <div><h1>Hello World!</h1></div>;
+const NotFound = () => <div><h1>Not Found</h1></div>;
 
 const router = () => (
   <Provider store={ store }>
     <Router history={ history }>
-      <Route path="/" component={ App }>
-        <IndexRoute />
-      </Route>
+      <Switch>
+        <Route exact path="/" component={ App } />
+        <Route component={ NotFound } />
+      </Switch>
     </Router>
   </Provider>
 );
 
 // eslint-disable-next-line no-undef
-render(router(), document.getElementById('app'));
+render(router(), document.getElementById('root'));
